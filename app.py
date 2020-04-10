fd2084741d634ce862f0315c7ef2436c40b79448
danil31219as <57491208+danil31219as@users.noreply.github.com>
2020-04-10 13:21:31 +0400

    города (общая функция)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 # импортируем функции из нашего второго файла geo
 from flask_ngrok import run_with_ngrok
 
-from geo import get_country, get_distance, get_coordinates
+from geo import get_distance, get_geo_info
 
 app = Flask(__name__)
 run_with_ngrok(app)
@@ -39,16 +39,15 @@ def handle_dialog(res, req):
         return
     # Получаем города из нашего
     cities = get_cities(req)
-    print(cities)
     if not cities:
         res['response']['text'] = 'Ты не написал название не одного города!'
     elif len(cities) == 1:
         res['response']['text'] = 'Этот город в стране - ' + \
-                                  get_country(cities[0])
+                                  get_geo_info(cities[0], 'country')
 
     elif len(cities) == 2:
-        distance = get_distance(get_coordinates(
-            cities[0]), get_coordinates(cities[1]))
+        distance = get_distance(get_geo_info(
+            cities[0], 'coordinates'), get_geo_info(cities[1], 'coordinates'))
         res['response']['text'] = 'Расстояние между этими городами: ' + \
                                   str(round(distance)) + ' км.'
     else:
